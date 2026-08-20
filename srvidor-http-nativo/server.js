7758b985f0bb58e48237db51051d789af6229aa4
Geoh8747 <geovanna.vasconcelos@estudante.ifms.edu.br>
2026-08-20 17:57:05 +0000

    exercicio 3

--- srvidor-http-nativo/server.js
+++ srvidor-http-nativo/server.js
@@ -1,14 +1,32 @@
-const http = require('node:http')
-const PORTA = 3000
+import http from 'node:http'
+import { URL } from 'node:url'
+
+const porta = 3000
+
+const produtos = [
+    {id: 1, nome: "Sabonete"},
+    {id: 2, nome: "Volante LogiTech G923"},
+    {id: 3, nome: "Sabão em Pó"},
+    {id: 4, nome: "Pelúcia do Sonic"},
+]
+
 const server = http.createServer((req, res) => {
-    console.log(`Requisição recebida! ${req.method} ${req.url}`)
-
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    console.log(new Date().toISOString())
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
 
-    res.end("Servidor nativo funcionando!");
+    if (req.method == "GET" && req.url == "/contato") {
+        return res.end(JSON.stringify({data:
+            {numero_telefone: "67 99999 9999",
+                endereco: "Rua da Alegria, 99, Centro"}}));
+    } 
+
+    if (req.method == "GET" && req.url == "/produtos") {
+        return res.end(JSON.stringify(produtos));
+    }
+
+    res.end(JSON.stringify({data: "Página Inicial"}))
 })
-1
-server.listen(PORTA, () => {console.log(`Servidor funcionando na porta ${PORTA}`)
-})
+
+server.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`)
+});
